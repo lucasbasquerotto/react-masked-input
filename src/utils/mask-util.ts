2356fbb16c0c04044a119af8141c7718b9dad4d5
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2023-04-06 18:28:01 -0300

    Update mask-util.ts

--- src/utils/mask-util.ts
+++ src/utils/mask-util.ts
@@ -54,3 +54,99 @@ export const createDynamicNumberMaskGenerator = (
 	rules: DEFAULT_MASK_RULES,
 	generateMask: (v) => dynamicNumberMask(v, ...masks),
 });
+
+export const getCurrencyMaskGenerator = ({
+	prefix = '',
+	thousandSeparator = '',
+	centsSeparator = '',
+}: {
+	prefix?: string;
+	thousandSeparator?: string;
+	centsSeparator?: string;
+}): MaskGenerator => {
+	const getRawValue = (value: string): string => {
+		const valNoPrefix = value?.startsWith(prefix)
+			? value?.substring(prefix.length)
+			: value;
+
+		const valNoCents = centsSeparator
+			? valNoPrefix?.replaceAll(centsSeparator, '')
+			: valNoPrefix;
+
+		const valDigits = thousandSeparator
+			? valNoCents?.replaceAll(thousandSeparator, '')
+			: valNoCents;
+
+		return valDigits ?? '';
+	};
+
+	return {
+		rules: DEFAULT_MASK_RULES,
+		generateMask: (value) => {
+			const rawVal = getRawValue(value);
+			const len = rawVal.length;
+			const lenCents = centsSeparator ? 2 : 0;
+			const lenNoCents = Math.max(len - lenCents, 0);
+
+			const mask =
+				prefix +
+				'9'.repeat(lenNoCents % 3) +
+				(lenNoCents % 3 > 0 && lenNoCents >= 3 ? thousandSeparator : '') +
+				(lenNoCents >= 3
+					? Array(Math.trunc(lenNoCents / 3))
+							.fill('999')
+							.join(thousandSeparator)
+					: '') +
+				centsSeparator +
+				'9'.repeat(lenCents);
+
+			return mask;
+		},
+		transform: (value) => {
+			const valDigits = getRawValue(value);
+			const rawVal = valDigits?.replace(/^0+/, '');
+			const len = rawVal.length;
+			const prefixToUse = value?.startsWith(prefix) ? prefix : '';
+
+			if (centsSeparator && len < 3) {
+				return (
+					prefixToUse + '0' + centsSeparator + '0'.repeat(2 - len) + rawVal
+				);
+			} else if (valDigits?.length !== rawVal?.length) {
+				const initial: {
+					current: string;
+					parts: string[];
+				} = { current: '', parts: [] };
+
+				const { parts: thousandsParts } = rawVal
+					.substring(0, len - 2)
+					.split('')
+					.reverse()
+					.reduce(({ current, parts }, char, idx, arr) => {
+						const newCurrent = char + current;
+
+						if (idx === arr?.length - 1 || (idx + 1) % 3 === 0) {
+							return {
+								current: '',
+								parts: [newCurrent, ...parts],
+							};
+						}
+
+						return {
+							current: newCurrent,
+							parts,
+						};
+					}, initial);
+
+				const valNoCents = thousandsParts.join(thousandSeparator);
+
+				const newValue =
+					prefixToUse + valNoCents + centsSeparator + rawVal.substring(len - 2);
+
+				return newValue;
+			}
+
+			return value;
+		},
+	};
+};
