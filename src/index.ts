3d6f8c9dafab3f4f042c3650659b01e23d546db1
Lucas Basquerotto <lucas@99freelas.com.br>
2023-04-13 18:24:19 -0300

    Add react native mask hook and release new version

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from './utils/mask-util';
 export * from './hooks/use-mask';
 export * from './hooks/use-ref-mask';
 export * from './hooks/use-web-mask';
+export * from './hooks/use-native-mask';
 export * from './components/masked-input';
